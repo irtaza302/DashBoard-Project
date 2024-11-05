08c5e00bff3c216a5d6c45933272ebdea6bc762c
Irtaza malik <86052250+irtaza302@users.noreply.github.com>
2024-11-05 15:54:21 +0500

    sas

--- config/database.js
+++ config/database.js
@@ -6,28 +6,13 @@ const connectDB = async () => {
       ? process.env.MONGODB_URI 
       : "mongodb+srv://malikirtaza302:GiBT8ireb7dVelsv@cluster0.yxagc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
-    await mongoose.connect(uri, {
-      serverSelectionTimeoutMS: 5000,
-      retryWrites: true,
-      w: 'majority',
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      connectTimeoutMS: 10000
-    });
-
+    await mongoose.connect(uri);
     console.log('MongoDB Connected Successfully');
-
-    mongoose.connection.on('error', err => {
-      console.error('MongoDB connection error:', err);
-    });
-
-    mongoose.connection.on('disconnected', () => {
-      console.log('MongoDB disconnected');
-    });
-
+    
+    return mongoose.connection;
   } catch (error) {
     console.error('MongoDB connection error:', error);
-    process.exit(1);
+    throw error;
   }
 };
 
